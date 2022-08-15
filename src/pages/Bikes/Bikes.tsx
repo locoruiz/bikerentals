5289473cf052a82350c30a587a67d03e7e8ee71d
Ricardo Ruiz <ricardoruiz@axon.com.bo>
2022-08-15 15:49:15 -0400

    Display users who reserved a bike in the bikes page

--- src/pages/Bikes/Bikes.tsx
+++ src/pages/Bikes/Bikes.tsx
@@ -6,7 +6,14 @@ import { BikeType } from '../../utils/types'
 import useApi from '../../hooks/useApi'
 import Loader from '../../components/Loader/Loader'
 import Modal from '../../components/Modal/Modal'
+import LoadPage from '../../components/LoadPage/LoadPage'
 
+
+type UsageType = {
+    username: string,
+    fromDate: string,
+    toDate: string
+}
 
 export interface IBikesProps {}
 
@@ -20,6 +27,9 @@ const Bikes: React.FC<IBikesProps> = (props) => {
     // Only available bikes
     const [bikes, setBikes] = useState<BikeType[]>([])
     const [editingBike, setEditingBike] = useState<BikeType | undefined>()
+    const [showUsersModal, setShowUsersModal] = useState(false)
+    const [users, setUsers] = useState<UsageType[]>([])
+    const [pageLoad, setPageLoad] = useState(false)
     const { loading, fetch } = useApi()
     const [formVisible, setFormVisible] = useState(false)
 
@@ -31,7 +41,7 @@ const Bikes: React.FC<IBikesProps> = (props) => {
         },
         validationSchema,
         onSubmit: values => {
-            closeForm()
+            setPageLoad(true)
             if (editingBike) {
                 fetch('bikes/'+editingBike.id, values, 'PUT')
                 .then((res) => {
@@ -41,20 +51,24 @@ const Bikes: React.FC<IBikesProps> = (props) => {
                         }
                         return bike
                     }))
+                    closeForm()
                 })
                 .catch(e => {
                     console.log(e)
                     alert('There was a problem, please try again later')
                 })
+                .finally(() => {setPageLoad(false)})
             } else {
                 fetch('bikes', values, 'POST')
                 .then((res) => {
                     setBikes([res.data, ...bikes])
+                    closeForm()
                 })
                 .catch(e => {
                     console.log(e)
                     alert('There was a problem, please try again later')
                 })
+                .finally(() => {setPageLoad(false)})
             }
         }
     })
@@ -95,7 +109,29 @@ const Bikes: React.FC<IBikesProps> = (props) => {
         formik.resetForm();
     }
 
+    const fetchUsers = (bikeId: number) => {
+        setPageLoad(true)
+        fetch(`bikes/${bikeId}/users`)
+            .then(res => {
+                if (res.data.length > 0) {
+                    setUsers(res.data)
+                    setShowUsersModal(true)
+                } else {
+                    alert('This bike was never reserved')
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                alert('There was an error fetching the users, please try again later')
+            })
+            .finally(() => {setPageLoad(false)})
+    }
+
     return (
+        <>
+        {
+            pageLoad && <LoadPage loading={pageLoad}/>
+        }
         <div className={styles.container}>
             <div className={styles.header}>
                 <h1>Registered Bikes</h1>
@@ -106,8 +142,8 @@ const Bikes: React.FC<IBikesProps> = (props) => {
                 </button>
             </div>
             {
-                loading || bikes.length === 0 ?
-                loading ? <Loader/> : <h2>There are no bikes registered</h2>
+                (loading && pageLoad === false) || bikes.length === 0 ?
+                loading && pageLoad === false ? <Loader/> : <h2>There are no bikes registered</h2>
                 :
                 <table className={'table '+styles.table}>
                     <tbody>
@@ -116,7 +152,7 @@ const Bikes: React.FC<IBikesProps> = (props) => {
                             <th>Model</th>
                             <th>Color</th>
                             <th>Location</th>
-                            <th>Rate</th>
+                            <th title='rating'>R.</th>
                             <th title='Available today'>A.</th>
                             <th></th>
                         </tr>
@@ -135,6 +171,15 @@ const Bikes: React.FC<IBikesProps> = (props) => {
                                         className='btn btn-primary btn-small'
                                         >
                                             Edit
+                                    </button>
+                                    &nbsp;
+                                    <button 
+                                        onClick={() => {fetchUsers(+bike.id)}}
+                                        type={'button'}
+                                        className='btn btn-small'
+                                        title='Users who reserved it'
+                                        >
+                                            Show users
                                     </button>
                                 </td>
                             </tr>)
@@ -228,7 +273,39 @@ const Bikes: React.FC<IBikesProps> = (props) => {
                     </div>
                 </form>
             </Modal>
+            <Modal 
+                title={ 'Users who used reserved this bike' }
+                visible={showUsersModal}
+                onClose={() => {setShowUsersModal(false)}}
+                footer={
+                    <div className={styles.modalFooter}>
+                        <button 
+                            onClick={() => {setShowUsersModal(false)}}
+                            className='btn btn-primary btn-small'>Close</button>
+                    </div>
+                }
+                >
+                <table className='table'>
+                    <thead style={{width: users.length <= 11 ? '100%' : 'calc(100% - 17px)'}}>
+                        <tr>
+                            <th>Username</th>
+                            <th>From</th>
+                            <th>To</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => <tr key={user.username}>
+                                <td>{user.username}</td>
+                                <td style={{textAlign: 'center'}}>{user.fromDate.substring(0, 10)}</td>
+                                <td style={{textAlign: 'center'}}>{user.toDate.substring(0, 10)}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </Modal>
         </div>
+        </>
     )
 }
 
